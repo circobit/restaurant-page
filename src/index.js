157434b20bc5593eec1e11ade190a54c30d5dc2f
Cristian Cevasco <cristian.cevasco@gmail.com>
2026-07-20 16:48:22 +0200

    add function switchTab(loadFunction) to index.js and set loadHome as default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // index.js
 import "./style.css";
 import { createHeader, createContentWrapper, createFooter } from "./modules/initial-load.js";
+import loadHome from "./modules/home.js";
 
 // Get #content div
 const contentDiv = document.getElementById("content");
@@ -11,3 +12,19 @@ contentDiv.appendChild(createHeader());
 contentDiv.appendChild(createContentWrapper());
 // Append footer to #content div
 contentDiv.appendChild(createFooter());
+
+// Load default init tab when loading the page for the first time
+const main = document.getElementById("mainContent");
+main.appendChild(loadHome());
+
+// Function to load content based on passed function
+function switchTab(loadFunction) {
+    // Clean current content from contentDiv
+    main.innerHTML = "";
+    // Append content from passed module as function
+    main.appendChild(loadFunction());
+}
+
+// Set event listener to nav buttons
+const homeBtn = document.getElementById("homeBtn");
+homeBtn.addEventListener("click", () => switchTab(loadHome));
